4ea814af86b089694f5f2832258cbbb61b464142
TrendHub <trendhub.biz@gmail.com>
2026-09-08 19:28:23 +0900

    Run296: include editorial format v2 in publication policy fingerprint

--- publication_contract.py
+++ publication_contract.py
@@ -79,6 +79,7 @@ PUBLICATION_POLICY_FILES = (
     "run182_eyecatch_conclusion_emphasis.py",
     "run183_eyecatch_emphasis_scale.py",
     "run222_note_presentation_integrity.py",
+    "run296_editorial_format_v2.py",
     "run248_first_real_publish_quality_calibration.py",
     "run249_final_publication_surface_gate.py",
     "publication_contract.py",
